cb5d964d8debd2372f14c01b03fb41852d07256d
Eric Figura <ericfigura@gmail.com>
2020-09-26 13:33:55 -0500

    Initial Commit

---
+++ FetchRewardsTests/FetchRewardsTests.swift
@@ -0,0 +1,34 @@
+//
+//  FetchRewardsTests.swift
+//  FetchRewardsTests
+//
+//  Created by Eric Figura on 9/26/20.
+//  Copyright © 2020 FetchRewards. All rights reserved.
+//
+
+import XCTest
+@testable import FetchRewards
+
+class FetchRewardsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
